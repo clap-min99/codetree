3b9f97fc12ee25150864405184e12e4b105a5995
박수민 <qkrtnals032@gmail.com>
2026-05-26 17:39:44 +0900

    [Passed] 굉장한 수 (1ms, 0MB)

---
+++ trail0/굉장한 수/amazing-number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int N;
+    scanf("%d", &N);
+    if ((N%2 == 1 && N%3 == 0) || (N%2 == 0 && N%5 == 0)) {
+        printf("true");
+    }
+    else {
+        printf("false");
+    }
+    return 0;
+}
