78db3b5ac73ca397974959dc568bff3478c690ac
박수민 <qkrtnals032@gmail.com>
2026-05-26 16:26:17 +0900

    [Passed] 문자열 출력하기 (1ms, 0MB)

---
+++ trail0/문자열 출력하기/print-string.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int N;
+    scanf("%d", &N);
+    int i = 0;
+    for (i = 0; i<N; i++) {
+        printf("LeebrosCode\n");
+    }
+    return 0;
+}
